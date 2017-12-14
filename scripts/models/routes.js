f607f54f7fda9aedf635aba0a9dbb35c2772168f
kbperez <kbpgperez@hotmail.com>
2017-12-14 10:50:39 -0800

    route.js catch all

--- scripts/models/routes.js
+++ scripts/models/routes.js
@@ -1,4 +1,4 @@
 page('/', ctx => app.Book.fetchAll(app.bookView.initIndexPage));
 page('/books/new', ctx => app.bookView.initCreateFormPage(ctx));
 page('/books/:book_id', ctx => app.Book.fetchOne(ctx, app.bookView.initDetailPage));
-page();
+page('/*', ctx => app.Book.fetchAll(app.bookView.initIndexPage));
